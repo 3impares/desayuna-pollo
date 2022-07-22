7e621b75d0144e4b7f218f14a96486174642fd51
SuperDabicho <dvd.92@hotmail.com>
2022-07-23 01:37:30 +0200

    load sprites en selección de personaje

--- Assets/scripts/NumPlayers.cs
+++ Assets/scripts/NumPlayers.cs
@@ -12,6 +12,11 @@ public class NumPlayers : MonoBehaviour
     private int numplayers;
     private int currentPlayer;
     public GameObject nickText;
+
+    private string[] filesSkins;
+    public GameObject skinPrefab;
+
+
     public void selectNumPlayers(int num)
     {
         numplayers = num;
@@ -26,6 +31,8 @@ public class NumPlayers : MonoBehaviour
         currentPlayer = 0;
         canvasPlayer.SetActive(true);
         currentText.text = "Player " + (int)(currentPlayer + 1);
+
+        loadSkins();
     }
 
     public void playerReady()
@@ -37,6 +44,20 @@ public class NumPlayers : MonoBehaviour
         
         
         //skin
+        Skin[] listaSkins;
+        GameObject div_skins = GameObject.Find("skins");
+
+        listaSkins = div_skins.GetComponentsInChildren<Skin>();
+        foreach(Skin s in listaSkins){
+            if(s.chosen){
+                print("Seleccionado la skin: " + s.img.sprite);
+                s.locked = true;
+                s.chosen = false;
+                players[currentPlayer].skin = s.img.sprite;
+                break;
+            } //incompleto, falta la comunicación a DataPLayer
+        }
+        print("Seleccionada la skin: " + players[currentPlayer].skin);
         
         
         if (currentPlayer == numplayers-1)
@@ -50,4 +71,29 @@ public class NumPlayers : MonoBehaviour
             currentText.text = "Player " + (int)(currentPlayer + 1);
         }
     }
+    
+
+    private void loadSkins(){
+
+        string path = @"Assets/Resources/skins/";
+        
+        filesSkins = System.IO.Directory.GetFiles(path, "*.asset");
+        print(filesSkins[0]); //Aquí sí lo coge
+        
+        GameObject div_skins = GameObject.Find("skins");
+
+        foreach(string filePath in filesSkins){
+            GameObject newObj = Instantiate(skinPrefab, div_skins.transform);
+            //div_skins.AddComponent(typeof(Image));
+            print(filePath.Replace("Assets/Resources/","").Replace(".asset",""));
+            Sprite spriteLoad = Resources.Load<Sprite>(filePath.Replace("Assets/Resources/","").Replace(".asset",""));
+            newObj.GetComponent<Image>().sprite = spriteLoad;
+
+            
+        }
+
+    }
+
+
+
 }
---
+++ Assets/scripts/Skin.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Skin : MonoBehaviour
+{
+    public bool locked;
+    public bool chosen;
+    public Image img;
+    // Start is called before the first frame update
+    void Start()
+    {
+        locked = false;
+        chosen = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(locked){
+            GetComponent<Button>().interactable = false;
+        }
+    }
+
+    public void chooseSkin()
+    {
+        chosen = true;
+    }
+
+}
