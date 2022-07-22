2f6c61d9bc1421478216758d84020f0baa8f0bce
marcos <laina855@gmail.com>
2022-07-22 22:24:00 +0200

    features canvas

--- Assets/scripts/NumPlayers.cs
+++ Assets/scripts/NumPlayers.cs
@@ -1,33 +1,53 @@
+
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NumPlayers : MonoBehaviour
 {
     public GameObject canvasNumber;
     public GameObject canvasPlayer;
-    public GameObject playerPrefab;
+    public TextMeshProUGUI currentText;
     public DataPlayer[] players;
     private int numplayers;
-    public int currentPlayer;
-    public TextMeshPro nickText;
+    private int currentPlayer;
+    public GameObject nickText;
     public void selectNumPlayers(int num)
     {
         numplayers = num;
         canvasNumber.SetActive(false);
-
+        players = new DataPlayer[num];
+        
         for (var i = 0; i < num; i++)
         {
             players[i] = new DataPlayer(i);
         }
 
         currentPlayer = 0;
-        
         canvasPlayer.SetActive(true);
+        currentText.text = "Player " + (int)(currentPlayer + 1);
     }
 
     public void playerReady()
     {
-        players[currentPlayer].nickname = nickText.text;
+        //nick
+        players[currentPlayer].nickname = nickText.GetComponent<TMP_InputField>().text;
+
+        //keys
         
+        
+        //skin
+        
+        
+        if (currentPlayer == numplayers-1)
+        {
+            ChangeScene.loadScene("Game");
+        }else{
+            nickText.GetComponent<TMP_InputField>().Select();
+            nickText.GetComponent<TMP_InputField>().text = "";
+            
+            currentPlayer++;
+            currentText.text = "Player " + (int)(currentPlayer + 1);
+        }
     }
 }
